12b9265b54dfb40fa82df5cdc40eeaaeabdb1717
Golam Kibria <kibriarobin97@gmail.com>
2026-05-21 23:21:31 +0600

    create database table

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-import express, { type Request, type Response } from "express";
-const app = express();
-const port = 5000;
+import express, { type Application, type Request, type Response } from "express";
+import config from "./config";
+import { initDB } from "./db/db";
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.status(200).json({ message: "Hello, DevPulse!", author: "Robin" });
 });
 
-app.listen(port, () => {
-  console.log(`DevPulse running on port ${port}`);
+app.listen(config.port, () => {
+  initDB();
+  console.log(`DevPulse running on port ${config.port}`);
 });
