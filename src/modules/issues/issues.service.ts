a98266c7807366135ebf813680710f01c904876e
Golam Kibria <kibriarobin97@gmail.com>
2026-05-22 16:11:31 +0600

    added create isshue

---
+++ src/modules/issues/issues.service.ts
@@ -0,0 +1,15 @@
+import { pool } from "../../db/db";
+import type { ICreateIssue } from "../../type";
+
+const createIssue = async (payload: ICreateIssue, reporterId: number) => {
+  const { title, description, type } = payload;
+
+  const result = await pool.query(
+    `INSERT INTO issues(title, description, type, reporter_id)
+     VALUES($1, $2, $3, $4)
+     RETURNING *`,
+    [title, description, type, reporterId],
+  );
+
+  return result.rows[0];
+};
